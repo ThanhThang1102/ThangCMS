2eccae49d933fae7df0d1936dda4e2eb1bf51584
ThanhThang1102 <buithanhthang11022003@gmail.com>
2026-05-23 21:26:41 +0700

    tạo project

---
+++ CMS.Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CMS.Data
+{
+    public class ApplicationDbContextFactory
+        : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                "Server=DESKTOP-SJKPVQV;Database=ThangCMS_DB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
